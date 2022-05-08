bd0d433491a3db15b5630fd6be33cb6c3f4434b6
Franklin Ramos <franklinramos@outlook.com>
2022-05-08 18:00:20 -0300

    commit bomba

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import CloseButton from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import FeedbackTypeStep from "./Steps/FeedbackTypeStep";
+import FeedbackContentStep from "./Steps/FeedbackContentStep";
+import FeedbackSuccessStep from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "imagem de um balão",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export default function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function resetFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onResetFeedback={resetFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              resetFeedbackType={resetFeedback}
+              feedbackType={feedbackType}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+}
